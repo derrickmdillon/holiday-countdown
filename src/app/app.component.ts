2efa786b6f396a3fd04c69dbbc678db375f93805
Derrick <derrickmdillon@gmail.com>
2020-01-21 21:42:15 +0000

    Working countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'holiday-countdown';
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+
+  constructor() {
+    const countDown = setInterval(() => {
+      const countDownDate = new Date('May 3, 2020 14:00:00').getTime();
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (this.days === 0) {
+        clearInterval(countDown);
+      }
+    }, 1000);
+  }
 }
